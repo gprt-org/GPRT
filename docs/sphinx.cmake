2e6bdedd5d503b2b5918c78008a106d5a1b5fbe2
natevm <natemorrical@gmail.com>
2022-11-03 18:39:35 -0600

    incorporating sphinx into cmake

---
+++ docs/sphinx.cmake
@@ -0,0 +1,24 @@
+cmake_minimum_required(VERSION 2.8.12)
+
+message("Executing command to generate documentation")
+execute_process(
+    COMMAND make html
+    WORKING_DIRECTORY ${DOC_SOURCE_DIR}
+    RESULT_VARIABLE RESULT
+    OUTPUT_VARIABLE OUTPUT
+    ERROR_VARIABLE ERROR
+)
+
+if (${RESULT} EQUAL 2)
+message("Error: Please verify that python, sphinx, sphinx_rtd_theme, and breathe packages are installed to build documentation")
+# else()
+endif()
+
+# message(${RESULT})
+message(${OUTPUT})
+
+
+if (ERROR)
+message(${ERROR})
+endif()
+
