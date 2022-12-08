a0e380d6ecc0655856726f24b37d38d85eb710b3
n8vm <natemorrical@gmail.com>
2022-12-07 17:24:05 -0700

    updates to sample 5

--- samples/s05-computeVertex/hostCode.cpp
+++ samples/s05-computeVertex/hostCode.cpp
@@ -41,15 +41,9 @@
   std::cout << "#gprt.sample(main): " << message << std::endl;   \
   std::cout << GPRT_TERMINAL_DEFAULT;
 
-extern GPRTProgram int05_deviceCode;
-
 const int GRID_SIDE_LENGTH = 100;
-float transform[3][4] = 
-  {
-    1.0f, 0.0f, 0.0f, 0.0f,
-    0.0f, 1.0f, 0.0f, 0.0f,
-    0.0f, 0.0f, 1.0f, 0.0f
-  };
+
+extern GPRTProgram s05_deviceCode;
 
 // initial image resolution
 const int2 fbSize = {1400, 460};
@@ -57,18 +51,49 @@ GLuint fbTexture {0};
 
 float3 lookFrom = {1.5f,-1.5f,1.f};
 float3 lookAt = {0.5f,0.5f,0.f};
-float3 lookUp = {0.f,0.f,1.f};
+float3 lookUp = {0.f,0.f,-1.f};
 float cosFovy = 0.45f;
 
 #include <iostream>
 int main(int ac, char **av)
 {
   // create a context on the first device:
+  gprtRequestWindow(fbSize.x, fbSize.y, "S05 Compute Vertex");
   GPRTContext context = gprtContextCreate(nullptr,1);
-  GPRTModule module = gprtModuleCreate(context,int05_deviceCode);
+  GPRTModule module = gprtModuleCreate(context,s05_deviceCode);
+
+  // ##################################################################
+  // set up all the GPU kernels we want to run
+  // ##################################################################
 
   // -------------------------------------------------------
   // Setup programs and geometry types
+  // -------------------------------------------------------
+  GPRTVarDecl trianglesGeomVars[] = {
+    { "index",  GPRT_BUFFER, GPRT_OFFSETOF(TrianglesGeomData,index)},
+    { "vertex",  GPRT_BUFFER, GPRT_OFFSETOF(TrianglesGeomData,vertex)},
+    { "now",  GPRT_FLOAT, GPRT_OFFSETOF(TrianglesGeomData,now)},
+    { "gridSize",  GPRT_UINT, GPRT_OFFSETOF(TrianglesGeomData,gridSize)},
+    { /* sentinel to mark end of list */ }
+  };
+  GPRTGeomType trianglesGeomType
+    = gprtGeomTypeCreate(context,
+                        GPRT_TRIANGLES,
+                        sizeof(TrianglesGeomData),
+                        trianglesGeomVars,-1);
+  gprtGeomTypeSetClosestHitProg(trianglesGeomType,0,
+                           module,"ClosestHit");
+  
+  // -------------------------------------------------------
+  // set up vertex program to animate vertices
+  // -------------------------------------------------------
+  GPRTCompute vertexProgram
+    = gprtComputeCreate(context,module,"Vertex",
+                      sizeof(TrianglesGeomData),
+                      trianglesGeomVars,-1);
+  
+  // -------------------------------------------------------
+  // set up ray gen program
   // -------------------------------------------------------
   GPRTVarDecl rayGenVars[] = {
     { "fbSize",        GPRT_INT2,   GPRT_OFFSETOF(RayGenData,fbSize)},
@@ -85,6 +110,9 @@ int main(int ac, char **av)
                       sizeof(RayGenData),
                       rayGenVars,-1);
 
+  // -------------------------------------------------------
+  // set up miss
+  // -------------------------------------------------------
   GPRTVarDecl missVars[]
     = {
     { "color0", GPRT_FLOAT3, GPRT_OFFSETOF(MissProgData,color0)},
@@ -95,31 +123,17 @@ int main(int ac, char **av)
     = gprtMissCreate(context,module,"miss",sizeof(MissProgData),
                         missVars,-1);
 
-  GPRTVarDecl trianglesGeomVars[] = {
-    { "index",  GPRT_BUFFER, GPRT_OFFSETOF(TrianglesGeomData,index)},
-    { "vertex",  GPRT_BUFFER, GPRT_OFFSETOF(TrianglesGeomData,vertex)},
-    { "now",  GPRT_FLOAT, GPRT_OFFSETOF(TrianglesGeomData,now)},
-    { "gridSize",  GPRT_UINT, GPRT_OFFSETOF(TrianglesGeomData,gridSize)},
-    { /* sentinel to mark end of list */ }
-  };
-  GPRTGeomType trianglesGeomType
-    = gprtGeomTypeCreate(context,
-                        GPRT_TRIANGLES,
-                        sizeof(TrianglesGeomData),
-                        trianglesGeomVars,-1);
-  gprtGeomTypeSetClosestHitProg(trianglesGeomType,0,
-                           module,"ClosestHit");
-  
-  GPRTCompute vertexProgram
-    = gprtComputeCreate(context,module,"Vertex",
-                      sizeof(TrianglesGeomData),
-                      trianglesGeomVars,-1);
-
+  // Note, we'll need to call this again after creating our acceleration 
+  // structures, as acceleration structures will introduce new shader 
+  // binding table records to the pipeline.
   gprtBuildPipeline(context);
 
-  // ------------------------------------------------------------------
-  // aabb mesh
-  // ------------------------------------------------------------------
+  // ##################################################################
+  // set the parameters for our triangle mesh and compute kernel
+  // ##################################################################
+  
+  // Note, the vertex and index buffers are empty here. We will fill 
+  // them in on the device by using our vertex program. 
   unsigned int numTriangles = 2 * GRID_SIDE_LENGTH * GRID_SIDE_LENGTH;
   unsigned int numVertices = 3 * numTriangles;
   GPRTBuffer vertexBuffer
@@ -129,47 +143,55 @@ int main(int ac, char **av)
   
   GPRTGeom trianglesGeom
     = gprtGeomCreate(context,trianglesGeomType);
+
+  // It is _okay_ to give our triangles geometry unpopulated buffers here
+  // for the vertices and indices, so long as they're filled in before 
+  // we go to build our acceleration structure.
   gprtTrianglesSetVertices(trianglesGeom, vertexBuffer, 
                            numVertices, sizeof(float3), 0);
   gprtTrianglesSetIndices(trianglesGeom, indexBuffer, 
                            numTriangles, sizeof(uint3), 0);
 
+  // Parameters for the geometry when a ray hits it.
   gprtGeomSetBuffer(trianglesGeom,"vertex",vertexBuffer);
   gprtGeomSetBuffer(trianglesGeom,"index",indexBuffer);
   gprtGeomSet1f(trianglesGeom,"now",0.0f);
   gprtGeomSet1ui(trianglesGeom,"gridSize",GRID_SIDE_LENGTH);
 
+  // Parameters for our vertex program that'll animate our vertices
   gprtComputeSetBuffer(vertexProgram,"vertex",vertexBuffer);
   gprtComputeSetBuffer(vertexProgram,"index",indexBuffer);
   gprtComputeSet1f(vertexProgram,"now",0.0f);
   gprtComputeSet1ui(vertexProgram,"gridSize",GRID_SIDE_LENGTH);
-   
-  // compute triangles in parallel with a compute shader
+  
+  // Build the shader binding table to upload parameters to the device
   gprtBuildShaderBindingTable(context, GPRT_SBT_COMPUTE);
+   
+  // Now, compute triangles in parallel with a vertex compute shader
   gprtComputeLaunch1D(context,vertexProgram,numTriangles);
 
+  // Now that our vertex buffer and index buffer are filled, we can compute
+  // our triangles acceleration structure.
   GPRTAccel trianglesAccel = gprtTrianglesAccelCreate(context,1,&trianglesGeom);
   gprtAccelBuild(context, trianglesAccel);
 
-  // ------------------------------------------------------------------
-  // the group/accel for that mesh
-  // ------------------------------------------------------------------
-  GPRTBuffer transformBuffer
-    = gprtDeviceBufferCreate(context,GPRT_TRANSFORM,1,transform);
   GPRTAccel world = gprtInstanceAccelCreate(context,1,&trianglesAccel);
-  gprtInstanceAccelSet3x4Transforms(world, transformBuffer);
   gprtAccelBuild(context, world);
 
-  // ----------- set variables  ----------------------------
-  gprtMissSet3f(miss,"color0",0.1f,0.1f,0.1f);
-  gprtMissSet3f(miss,"color1",.0f,.0f,.0f);
+  // ##################################################################
+  // set the parameters for the rest of our kernels
+  // ##################################################################
 
-  // ----------- set raygen variables  ----------------------------
+  // Setup pixel frame buffer
   GPRTBuffer frameBuffer
     = gprtHostBufferCreate(context,GPRT_INT,fbSize.x*fbSize.y);
   gprtRayGenSetBuffer(rayGen,"fbPtr", frameBuffer);
   gprtRayGenSet2iv(rayGen,"fbSize", (int32_t*)&fbSize);
   gprtRayGenSetAccel(rayGen,"world", world);
+
+  // Miss program checkerboard background colors
+  gprtMissSet3f(miss,"color0",0.1f,0.1f,0.1f);
+  gprtMissSet3f(miss,"color1",.0f,.0f,.0f);
 
   // ##################################################################
   // build *SBT* required to trace the groups
@@ -180,27 +202,6 @@ int main(int ac, char **av)
   gprtBuildShaderBindingTable(context, GPRT_SBT_ALL);
 
   // ##################################################################
-  // create a window we can use to display and interact with the image
-  // ##################################################################
-  if (!glfwInit())
-    // Initialization failed
-    throw std::runtime_error("Can't initialize GLFW");
-
-  auto error_callback = [](int error, const char* description)
-  {
-    fprintf(stderr, "Error: %s\n", description);
-  };
-  glfwSetErrorCallback(error_callback);
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-  GLFWwindow* window = glfwCreateWindow(fbSize.x, fbSize.y, 
-    "Int02 Simple AABBs", NULL, NULL);
-  if (!window) throw std::runtime_error("Window or OpenGL context creation failed");
-  glfwMakeContextCurrent(window);
-
-  // ##################################################################
   // now that everything is ready: launch it ....
   // ##################################################################
 
@@ -209,20 +210,20 @@ int main(int ac, char **av)
   bool firstFrame = true;
   double xpos = 0.f, ypos = 0.f;
   double lastxpos, lastypos;
-  while (!glfwWindowShouldClose(window))
+  do
   {
     float speed = .001f;
     lastxpos = xpos;
     lastypos = ypos;
-    glfwGetCursorPos(window, &xpos, &ypos);
+    gprtGetCursorPos(context, &xpos, &ypos);
     if (firstFrame) {
       lastxpos = xpos;
       lastypos = ypos;
     }
-    int state = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+    int state = gprtGetMouseButton(context, GPRT_MOUSE_BUTTON_LEFT);
 
     // If we click the mouse, we should rotate the camera
-    if (state == GLFW_PRESS || firstFrame)
+    if (state == GPRT_PRESS || firstFrame)
     {
       firstFrame = false;
       float4 position = {lookFrom.x, lookFrom.y, lookFrom.z, 1.f};
@@ -233,7 +234,7 @@ int main(int ac, char **av)
 
       // step 1 : Calculate the amount of rotation given the mouse movement.
       float deltaAngleX = (2 * M_PI / fbSize.x);
-      float deltaAngleY = -(M_PI / fbSize.y);
+      float deltaAngleY = (M_PI / fbSize.y);
       float xAngle = (lastxpos - xpos) * deltaAngleX;
       float yAngle = (lastypos - ypos) * deltaAngleY;
 
@@ -272,62 +273,25 @@ int main(int ac, char **av)
     gprtComputeLaunch1D(context,vertexProgram,numTriangles);
     gprtAccelBuild(context, trianglesAccel);
     gprtAccelBuild(context, world);
-
     gprtRayGenSetAccel(rayGen, "world", world);
     gprtBuildShaderBindingTable(context, GPRT_SBT_HITGROUP);
+    
+    // Note! we don't need to rebuild the pipeline here, since no geometry was
+    // made or destroyed, only updated.
 
-    // Now, trace rays
+    // Calls the GPU raygen kernel function
     gprtRayGenLaunch2D(context,rayGen,fbSize.x,fbSize.y);
-
-    // Render results to screen
-    void* pixels = gprtBufferGetPointer(frameBuffer);
-    if (fbTexture == 0)
-      glGenTextures(1, &fbTexture);
     
-    glBindTexture(GL_TEXTURE_2D, fbTexture);
-    GLenum texFormat = GL_RGBA;
-    GLenum texelType = GL_UNSIGNED_BYTE;
-    glTexImage2D(GL_TEXTURE_2D, 0, texFormat, fbSize.x, fbSize.y, 0, GL_RGBA,
-                  texelType, pixels);
-
-    glDisable(GL_LIGHTING);
-    glColor3f(1, 1, 1);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, fbTexture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    
-    glDisable(GL_DEPTH_TEST);
-
-    glViewport(0, 0, fbSize.x, fbSize.y);
-
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.f, (float)fbSize.x, 0.0, (float)fbSize.y, -1.f, 1.f);
-
-    glBegin(GL_QUADS);
-    {
-      glTexCoord2f(0.f, 0.f);
-      glVertex3f(0.f, 0.f, 0.f);
-    
-      glTexCoord2f(0.f, 1.f);
-      glVertex3f(0.f, (float)fbSize.y, 0.f);
-    
-      glTexCoord2f(1.f, 1.f);
-      glVertex3f((float)fbSize.x, (float)fbSize.y, 0.f);
-    
-      glTexCoord2f(1.f, 0.f);
-      glVertex3f((float)fbSize.x, 0.f, 0.f);
-    }
-    glEnd();
-    
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    // If a window exists, presents the framebuffer here to that window
+    gprtBufferPresent(context, frameBuffer); 
   }
+  // returns true if "X" pressed or if in "headless" mode 
+  while (!gprtWindowShouldClose(context));
+
+  // Save final frame to an image
+  LOG("done with launch, writing frame buffer to " << outFileName);
+  gprtBufferSaveImage(frameBuffer, fbSize.x, fbSize.y, outFileName);
+  LOG_OK("written rendered frame buffer to file "<<outFileName);
 
   // ##################################################################
   // and finally, clean up
@@ -335,13 +299,9 @@ int main(int ac, char **av)
 
   LOG("cleaning up ...");
 
-  glfwDestroyWindow(window);
-  glfwTerminate();
-
   gprtBufferDestroy(vertexBuffer);
   gprtBufferDestroy(indexBuffer);
   gprtBufferDestroy(frameBuffer);
-  gprtBufferDestroy(transformBuffer);
   gprtRayGenDestroy(rayGen);
   gprtMissDestroy(miss);
   gprtComputeDestroy(vertexProgram);
